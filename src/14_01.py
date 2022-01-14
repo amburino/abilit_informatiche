a0634a9ab2fac969d7858a51521c26fdd68cfa52
Lorenzo Tamburino <95907682+amburino@users.noreply.github.com>
2022-01-14 09:56:44 +0000

    matrix multiplication

--- src/14_01.py
+++ src/14_01.py
@@ -1,28 +1,56 @@
+l = []
+with open('input1.txt', 'r') as f:
+  for line in f:
+    line = line.strip()
+    if len(line) > 0:
+     l.append(list(map(int, line.split(','))))
+#print(l)
+#################
+
+lettura= open("input1.txt", "r")
+a=[]
+for line in lettura.readlines():
+  a.append([int(x) for x in line.split(',')])
+#print(a)
+##################
+
 import math
 
-infile="mydata.dat"
-outfile="myout.dat"
+# 3x3 matrix
+X = [[12,7,3],[4 ,5,6],[7 ,8,9]]
+# 3x4 matrix
+Y = [[5,8,1,2],[6,7,3,0],[4,5,9,1]]
+# result is 3x4
+result = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+for i in range(len(X)):
+  for j in range(len(Y[0])):
+    for k in range(len(Y)):
+      result[i][j] += X[i][k] * Y[k][j]
+for r in result:
+  print(r)
+print("----------")
+result1= [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*Y)] for X_row in X]
+for r in result:
+  print(r)
 
-def f(y):
-  if y >= 0.0:
-    return y**5*math.exp(-y)
-  else:
-    return 0.0
-
-indata=open(infile, "r")
-linee=indata.readlines()
-indata.close()
-letti=[]
-x=[]
-for n in linee:
-  valori= n.split()
-  x.append(float(valori[0]));
-  y= float(valori[1])
-  letti.append(f(y))
-
-outdata= open(outfile, "w")
-i=0
-for n in letti:
-  outdata.write('%g %12.5e\n' %(x[i],n))
-  i+=1
-outdata.close()
+########################
+print("----------")
+#matrix x scalar showing AxB != BxA
+A = [[12,7,3],[4 ,5,6],[7 ,8,9]]
+# 3x4 matrix
+B = [[5,8,1,2],[6,7,3,0],[4,5,9,1]]
+def print_matrix(matrix):
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):   
+      print(str((matrix[i][j]))+"\t", end='')
+    print("\n")
+def matrix_x_matrix(X, Y):
+ result= [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
+ for i in range(len(X)):
+   for j in range(len(Y[0])):
+     for k in range(len(Y)):
+       result[i][j] += X[i][k] * Y[k][j]
+ return result
+print_matrix(matrix_x_matrix(A,B))
+print("------")
+print_matrix(matrix_x_matrix(B,A))
