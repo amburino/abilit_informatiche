8d99f1d5204f0335c906a359c35b8c2bb15a822a
Lorenzo Tamburino <95907682+amburino@users.noreply.github.com>
2022-01-21 08:44:51 +0000

    finding documentation

--- src/14_01.py
+++ src/14_01.py
@@ -1,56 +1,9 @@
-l = []
-with open('input1.txt', 'r') as f:
-  for line in f:
-    line = line.strip()
-    if len(line) > 0:
-     l.append(list(map(int, line.split(','))))
-#print(l)
-#################
-
-lettura= open("input1.txt", "r")
-a=[]
-for line in lettura.readlines():
-  a.append([int(x) for x in line.split(',')])
-#print(a)
-##################
-
-import math
-
-# 3x3 matrix
-X = [[12,7,3],[4 ,5,6],[7 ,8,9]]
-# 3x4 matrix
-Y = [[5,8,1,2],[6,7,3,0],[4,5,9,1]]
-# result is 3x4
-result = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-for i in range(len(X)):
-  for j in range(len(Y[0])):
-    for k in range(len(Y)):
-      result[i][j] += X[i][k] * Y[k][j]
-for r in result:
-  print(r)
-print("----------")
-result1= [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*Y)] for X_row in X]
-for r in result:
-  print(r)
-
-########################
-print("----------")
-#matrix x scalar showing AxB != BxA
-A = [[12,7,3],[4 ,5,6],[7 ,8,9]]
-# 3x4 matrix
-B = [[5,8,1,2],[6,7,3,0],[4,5,9,1]]
-def print_matrix(matrix):
-  for i in range(len(matrix)):
-    for j in range(len(matrix[i])):   
-      print(str((matrix[i][j]))+"\t", end='')
-    print("\n")
-def matrix_x_matrix(X, Y):
- result= [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
- for i in range(len(X)):
-   for j in range(len(Y[0])):
-     for k in range(len(Y)):
-       result[i][j] += X[i][k] * Y[k][j]
- return result
-print_matrix(matrix_x_matrix(A,B))
-print("------")
-print_matrix(matrix_x_matrix(B,A))
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+      print("Hello my name is " + self.name "and i'm " + self.age)
+      
+p1=Person("John", 36) 
+p1.myfunc()
---
+++ src/21_01.py
@@ -0,0 +1,15 @@
+import math 
+dir()
+help(math.acos)
+y=math.asin(0.5)
+print(y)
+l = [1, "alfa", 0.9, (1, 2, 3)]
+for i in l:
+ print (type(i))
+##########
+import scipy.optimize
+import numpy as np
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+from scipy import fftpack
+from scipy import integrate
